1117bfb9c5a2a532c5d467133175381f138e09cb
sudhanshu-881 <92862248+sudhanshu-881@users.noreply.github.com>
2025-09-05 14:43:09 -0700

    Delete tut-port directory

--- tut-port/src/lib/dbConnect.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-const MONGODB_URI = process.env.MONGODB_URI;
-export default async function dbConnect() {
-  if (mongoose.connection.readyState >= 1) return;
-  return mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-}
--- tut-port/src/models/question.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const QuestionSchema = new mongoose.Schema({
-  text: String,
-  options: [String],
-  answer: String, // correct answer
-  formatting: Object, // e.g. {type: "MCQ", section: "Math"}
-});
-export default mongoose.models.Question || mongoose.model("Question", QuestionSchema);
